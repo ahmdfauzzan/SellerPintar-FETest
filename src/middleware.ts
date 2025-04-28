b770abbbb57337b852ffe41fafbf1275612c6f59
ahmdfauzzan <ahamdfauzan.280.af@gmail.com>
2025-04-28 23:14:24 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/artikel/:path*", "/profile"],
+  matcher: ["/artikel/:path*", "/profile", "/admin"], 
 };
